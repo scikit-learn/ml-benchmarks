ad8fd8ebc5b17a15c3c452b8c90d78b244185fb0
Fabian Pedregosa <fabian.pedregosa@inria.fr>
2011-02-23 15:23:05 +0100

    Update PCA

--- benchmarks/bench_pca.py
+++ benchmarks/bench_pca.py
@@ -2,11 +2,6 @@
 
 import numpy as np
 from datetime import datetime
-from scikits.learn import pca as skl_pca
-from pybrain.auxiliary import pca as pybrain_pca
-import mdp
-from mvpa.mappers.pca import PCAMapper as MVPA_PCA
-from mvpa.datasets import Dataset
 
 #
 #       .. Load dataset ..
@@ -24,6 +19,7 @@ def bench_skl():
 #
 #       .. scikits.learn ..
 #
+    from scikits.learn import pca as skl_pca
     start = datetime.now()
     clf = skl_pca.RandomizedPCA(n_components=n_components)
     clf.fit(X)
@@ -34,6 +30,7 @@ def bench_pybrain():
 #
 #       .. pybrain ..
 #
+    from pybrain.auxiliary import pca as pybrain_pca
     start = datetime.now()
     pybrain_pca.pca(X, n_components)
     return datetime.now() - start
@@ -43,27 +40,47 @@ def bench_mdp():
 #
 #       .. MDP ..
 #
+    import mdp
     start = datetime.now()
     mdp.pca(X, output_dim=n_components)
     return datetime.now() - start
 
 
-def bench_mvpa():
+def bench_pymvpa():
 #
 #       .. PyMVPA ..
 #
+    from mvpa.mappers.pca import PCAMapper as MVPA_PCA
+    from mvpa.datasets import dataset_wizard
     start = datetime.now()
     clf = MVPA_PCA()
     print 'Warning, PyMVPA does not accept keyword to set number ' \
           'of components'
-    data = Dataset(samples=X, labels=0)
+    data = dataset_wizard(samples=X)
     clf.train(data)
     return datetime.now() - start
 
 
 if __name__ == '__main__':
-    print __doc__
-    print 'scikits.learn: ', bench(bench_skl), bench(bench_skl)
-    print 'pybrain: ', bench(bench_pybrain), bench(bench_pybrain)
-    print 'MDP: ', bench(bench_mdp), bench(bench_mdp)
-    print 'PyMVPA: ', bench(bench_mvpa), bench(bench_mvpa)
+
+    # don't bother me with warnings
+    import warnings; warnings.simplefilter('ignore')
+    np.seterr(all='ignore')
+
+    print __doc__ + '\n'
+
+    res_mdp = bench(bench_mdp)
+    print 'MDP: mean %s, std %s' % (
+        np.mean(res_mdp), np.std(res_mdp))
+
+    res_skl = bench(bench_skl)
+    print 'scikits.learn: mean %s, std %s' % (
+        np.mean(res_skl), np.std(res_skl))
+
+    res_pybrain = bench(bench_pybrain)
+    print 'Pybrain: mean %s, std %s' % (
+        np.mean(res_pybrain), np.std(res_pybrain))
+
+    res_pymvpa = bench(bench_pymvpa)
+    print 'PyMVPA: mean %s, std %s' % (
+        np.mean(res_pymvpa), np.std(res_pymvpa))
